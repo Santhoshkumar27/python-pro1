f25600258782a62e6824652ebe28849734f51476
santhosh kumar <75415950+Santhoshkumar27@users.noreply.github.com>
2022-02-02 09:04:25 +0530

    Update file_name.py

--- file_name.py
+++ file_name.py
@@ -1,3 +1,11 @@
-file_name = input("Input the Filename: ")
-f_extns = file_name.split(".")
-print ("The extension of the file is : " + repr(f_extns[-1]))
+
+import os
+  
+split_tup = os.path.splitext('myCaptain.py')
+print(split_tup)
+ 
+file_name = split_tup[0]
+file_extension = split_tup[1]
+  
+print("File Name: ", file_name)
+print("File Extension: ", file_extension)
